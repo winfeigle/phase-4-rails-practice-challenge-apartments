83b6ace2fc6150239c65cbc21f7c837759e76875
Win Feigle <win.feigle@gmail.com>
2023-01-05 18:00:23 -0700

    Complete challenge

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -1,2 +1,29 @@
 class LeasesController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def create
+        new_lease = Lease.create!(lease_params)
+        render json: new_lease, status: :created
+    end
+
+    def destroy
+        lease = Lease.find(params[:id])
+        lease.destroy()
+        head :no_content
+    end
+
+    private
+
+    def lease_params
+        params.permit(:rent, :apartment_id, :tenant_id)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def render_not_found_response
+        render json: { error: "Lease not found" }, status: :not_found
+    end
 end
--- app/models/lease.rb
+++ app/models/lease.rb
@@ -1,4 +1,6 @@
 class Lease < ApplicationRecord
+  validates :apartment_id && :tenant_id && :rent, presence: true
+
   belongs_to :apartment
   belongs_to :tenant
 end
