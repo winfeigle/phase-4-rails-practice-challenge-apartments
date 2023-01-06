d182089754a43b166eb7a3388906ac7f6277a675
Win Feigle <win.feigle@gmail.com>
2023-01-05 17:22:30 -0700

    Add routes for apartment, tenant, leases

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :leases
+  resources :leases, only: [:create, :destroy]
   resources :tenants
   resources :apartments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
